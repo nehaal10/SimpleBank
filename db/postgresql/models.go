265e6b0890ea51ac55754894b7781f38a4dfd9e8
nehaal <72294250+nehaal10@users.noreply.github.com>
2023-12-14 12:09:15 +0530

    ci init

--- db/postgresql/models.go
+++ db/postgresql/models.go
@@ -32,24 +32,3 @@ type Transfer struct {
 	Amount        int64       `json:"amount"`
 	CreatedAt     time.Time   `json:"created_at"`
 }
-
-type User struct {
-	Username          string    `json:"username"`
-	HashedPassword    string    `json:"hashed_password"`
-	FullName          string    `json:"full_name"`
-	Email             string    `json:"email"`
-	PasswordChangedAt time.Time `json:"password_changed_at"`
-	CreatedAt         time.Time `json:"created_at"`
-	Role              string    `json:"role"`
-	IsEmailVerified   bool      `json:"is_email_verified"`
-}
-
-type VerifyEmail struct {
-	ID         int64     `json:"id"`
-	Username   string    `json:"username"`
-	Email      string    `json:"email"`
-	SecretCode string    `json:"secret_code"`
-	IsUsed     bool      `json:"is_used"`
-	CreatedAt  time.Time `json:"created_at"`
-	ExpiredAt  time.Time `json:"expired_at"`
-}
